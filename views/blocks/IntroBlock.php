5b46545e7d5be81a3a02894896caa7cd84ed156e
Silvan <silvan@dev7.ch>
2018-02-12 11:59:06 +0100

    extend intro section block

--- views/blocks/IntroBlock.php
+++ views/blocks/IntroBlock.php
@@ -13,62 +13,70 @@
  */
 
 $bgImage = $this->extraValue('bgImage') ? $this->extraValue('bgImage')->source : 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
-$slides = $this->extraValue('slides');
+$slides = $this->extraValue('slides') ? $this->extraValue('slides') : false ;
 
 ?>
 
-<div class="intro-image py-5" style="background-image: url('<?= $bgImage ?>');">
+<div class="<?= $slides !== false ? 'intro-image-slider ' : 'intro-image ' ?>py-5" style="background-image: url('<?= $bgImage ?>');">
     <div class="container">
-        <div class="intro-slider">
-            <div class="slider">
-                <?php foreach ($slides as $slide): ?>
-                    <div class="slider-image" <?= $slide['image'] != null ? ' style="background-image: url(\'' . str_replace(' ', '/',($slide['image']->source)) . '\')"' : ''; ?>>
-                        <div class="slider-content">
-                            <h1 class="slider-title"><?= $slide['title'] ?></h1>
-                            <span class="slider-text m-0"><?= $slide['text'] ?></span>
-                            <? if ($slide['link'] != null): ?>
-                                <button type="button" class="btn btn-primary">
-                                    <a class="slider-link d-inline-block" href="<?= $slide['link'] ?>" style="color: inherit; text-decoration: inherit">Info</a>
-                                </button>
-                            <? endif; ?>
+        <? if ($slides !== false): ?>
+            <div class="intro-slider">
+                <div class="slider">
+                    <?php foreach ($slides as $slide): ?>
+                        <div class="slider-image" <?= $slide['image'] != null ? ' style="background-image: url(\'' . str_replace(' ', '/',($slide['image']->source)) . '\')"' : ''; ?>>
+                            <div class="slider-content">
+                                <h1 class="slider-title"><?= $slide['title'] ?></h1>
+                                <span class="slider-text m-0"><?= $slide['text'] ?></span>
+                                <? if ($slide['link'] != null): ?>
+                                    <button type="button" class="btn btn-primary">
+                                        <a class="slider-link d-inline-block" href="<?= $slide['link'] ?>" style="color: inherit; text-decoration: inherit">Info</a>
+                                    </button>
+                                <? endif; ?>
+                            </div>
                         </div>
-                    </div>
-                <?php endforeach; ?>
-                <?= $this->appView->registerJs('
-                        const IntroSlider = new Siema({
-                            selector: \'.slider\',
-                            duration: 200,
-                            easing: \'ease-out\',
-                            perPage: 1,
-                            startIndex: 0,
-                            draggable: true,
-                            multipleDrag: true,
-                            threshold: 20,
-                            loop: true,
-                            onInit: () => {},
-                            onChange: () => {},
-                        });
-                        document.querySelector(\'.intro-slider-prev\').addEventListener(\'click\', () => IntroSlider.prev());
-                        document.querySelector(\'.intro-slider-next\').addEventListener(\'click\', () => IntroSlider.next());
-                        ',
-                    \yii\web\View::POS_READY) ?>
+                    <?php endforeach; ?>
+                    <?= $this->appView->registerJs('
+                            const IntroSlider = new Siema({
+                                selector: \'.slider\',
+                                duration: 200,
+                                easing: \'ease-out\',
+                                perPage: 1,
+                                startIndex: 0,
+                                draggable: true,
+                                multipleDrag: true,
+                                threshold: 20,
+                                loop: true,
+                                onInit: () => {},
+                                onChange: () => {},
+                            });
+                            document.querySelector(\'.intro-slider-prev\').addEventListener(\'click\', () => IntroSlider.prev());
+                            document.querySelector(\'.intro-slider-next\').addEventListener(\'click\', () => IntroSlider.next());
+                            ',
+                        \yii\web\View::POS_READY) ?>
+                </div>
+                <div class="slider-nav d-flex">
+                    <button type="button" class="intro-slider-prev btn btn-link mr-suto"><i class="fas fa-chevron-left"></i></button>
+                    <button type="button" class="intro-slider-next btn btn-link ml-auto"><i class="fas fa-chevron-right"></i></button>
+                </div>
+                <?= $this->appView->registerJs('        
+                            intro.reveal(\'.slider\', {origin:\'top\', reset: false, move:2, scale:1.3, duration:600});
+                            intro.reveal(\'.slider-nav\', {origin:\'right\', reset: true, scale:.6, delay:600});
+                            ',
+                    \yii\web\View::POS_LOAD); ?>
             </div>
-            <div class="slider-nav d-flex">
-                <button type="button" class="intro-slider-prev btn btn-link mr-suto"><i class="fas fa-chevron-left"></i></button>
-                <button type="button" class="intro-slider-next btn btn-link ml-auto"><i class="fas fa-chevron-right"></i></button>
+        <? else: ?>
+            <div class="intro-text intro-text-with-bg py-5">
+                <?= $this->extraValue('text'); ?>
             </div>
-            <?= $this->appView->registerJs('        
-                        intro.reveal(\'.slider\', {origin:\'top\', reset: false, move:2, scale:1.3, duration:600});
-                        intro.reveal(\'.slider-nav\', {origin:\'right\', reset: true, scale:.6, delay:600});
-                        ',
-                \yii\web\View::POS_LOAD); ?>
-        </div>
+        <? endif; ?>
     </div>
 </div>
-<article class="intro-text-wrapper mt-5">
-    <div class="container">
-        <div class="intro-text pt-5">
-            <?= $this->extraValue('text'); ?>
+<? if ($slides !== false): ?>
+    <article class="intro-text-wrapper mt-5">
+        <div class="container">
+            <div class="intro-text pt-5">
+                <?= $this->extraValue('text'); ?>
+            </div>
         </div>
-    </div>
-</article>
+    </article>
+<? endif; ?>
