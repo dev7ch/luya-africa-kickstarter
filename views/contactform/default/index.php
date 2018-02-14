db99a679959798d831a4b89737eebd24fa4a45f3
Silvan <silvan@dev7.ch>
2018-02-14 16:41:36 +0100

    contact form layout

--- views/contactform/default/index.php
+++ views/contactform/default/index.php
@@ -10,17 +10,28 @@ use yii\helpers\Html;
 ?>
 
 <?php if (Yii::$app->session->getFlash('contactform_success')): ?>
-    <div class="alert alert-success">The form has been submited successfull.</div>
+    <div class="alert alert-success">The form has been submitted successfully.</div>
 <?php else: ?>
     <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'first_name')->textInput(); ?>
-    <?= $form->field($model, 'last_name')->textInput(); ?>
-    <?= $form->field($model, 'email'); ?>
-    <?= $form->field($model, 'phone'); ?>
-    <?= $form->field($model, 'message')->textarea(); ?>
-
-    <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
-
+    <div class="row">
+        <div class="col-12">
+            <?= $form->field($model, 'first_name')->textInput(); ?>
+        </div>
+        <div class="col-12">
+            <?= $form->field($model, 'last_name')->textInput(); ?>
+        </div>
+        <div class="col-12">
+            <?= $form->field($model, 'email'); ?>
+        </div>
+        <div class="col-12">
+            <?= $form->field($model, 'phone'); ?>
+        </div>
+        <div class="col-12">
+            <?= $form->field($model, 'message')->textarea(); ?>
+        </div>
+        <div class="col-12">
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+        </div>
+    </div>
     <?php ActiveForm::end(); ?>
 <?php endif; ?>
